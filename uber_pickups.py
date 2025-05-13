2dc6444e2d05472959868b185ae49fe4104f2335
Aidan <sba24450@student.cct.ie>
2025-05-13 19:07:02 +0100

    Update uber_pickups.py

--- uber_pickups.py
+++ uber_pickups.py
@@ -21,3 +21,8 @@ if st.checkbox('Show raw data'):
      st.subheader('Number of pickups by hour')
      hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
      st.bar_chart(hist_values)
+# Some number in the range 0-23
+hour_to_filter = st.slider('hour', 0, 23, 17)
+filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
+st.subheader('Map of all pickups at %s:00' % hour_to_filter)
+st.map(filtered_data)
